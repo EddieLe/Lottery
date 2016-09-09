a314ed704752c915a1a9439053327c0fb7429c9f
Eddie <eddie@Eddie>
2016-09-09 11:33:58 +0800

    while time

--- Lottery.php
+++ Lottery.php
@@ -5,42 +5,58 @@ ini_set("display_errors",true);
 function doLottery()
 {
 //    ignore_user_abort(true);
-//
-
-//    $mypdo = new MyPDO();
-//    $pdo = $mypdo->pdoConnect;
-//
-//    for ($i = 1; $i <= 10; $i++) {
-//        $times = substr((1000 + $i),-3);
-//        echo $gameId = date("Ymd") . "-" . $times;
-//        echo $startTime = time() + (120 * $i) . "<br>";
-//        echo $endTime = ($startTime + 120) . "<br>";
-//        echo $stopTime = ($endTime - 60) . "<br>";
-//
-//
-//        $sql = "INSERT INTO `Lottery`(`gameID`, `startTime`, `endTime`, `stopTime`) VALUES (:gameId, :startTime, :endTime, :stopTime)";
-//        $stmt = $pdo->prepare($sql);
-//        $stmt->execute([':gameId' => $gameId,
-//            ':startTime' => $startTime,
-//            ':endTime' => $endTime,
-//            ':stopTime' => $stopTime
-//        ]);
-//
-//    }
-//    while (true) {
-    for ($i = 0; $i < 10; $i++) {
-        $rand[] = $i;
-    }
-    shuffle($rand);
-    $number = array_slice($rand, 0, 5);
-    echo json_encode($number);
-
+    $flag = true;
     $mypdo = new MyPDO();
     $pdo = $mypdo->pdoConnect;
-    $sql = "INSERT INTO `Lottery`(`number`) VALUES (:number) WHERE ";
-    $pdo->query($sql);
-    $pdo->execute([':number' => $number]);
-//}
+
+    for ($i = 1; $i <= 3; $i++) {
+        $times = substr((1000 + $i),-3);
+        $gameId = date("Ymd") . "-" . $times;
+        $startTime = time() + (120 * ($i - 1));
+        $endTime = ($startTime + 120);
+        $stopTime = ($endTime - 60);
+        echo date("Y-m-d H:i:s", $startTime) . "<br>";
+        echo date("Y-m-d H:i:s", $endTime) . "<br>";
+        echo date("Y-m-d H:i:s", $stopTime) . '<br>';
+
+//echo time();
+        $sql = "INSERT INTO `Lottery`(`gameID`, `startTime`, `endTime`, `stopTime`) VALUES (:gameId, :startTime, :endTime, :stopTime)";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([':gameId' => $gameId,
+            ':startTime' => $startTime,
+            ':endTime' => $endTime,
+            ':stopTime' => $stopTime
+        ]);
+
+    }
+    while ($flag) {
+        for ($i = 0; $i < 10; $i++) {
+            $rand[] = $i;
+        }
+        shuffle($rand);
+        $result = array_slice($rand, 0, 5);
+        $number = json_encode($result);
+
+        $mypdo = new MyPDO();
+        $pdo = $mypdo->pdoConnect;
+        $sql = "SELECT `startTime` FROM `Lottery`";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+        $row =$stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        for ($i = 0; $i < count($row); $i++) {
+            if ($row[$i]['startTime'] == time()) {
+    //            echo 123;
+                $sql = "UPDATE `Lottery` SET `number` = :number WHERE `startTime` = :startTime";
+                $stmt = $pdo->prepare($sql);
+                $stmt->execute([':number' => $number, ':startTime' => $row[$i]['startTime']]);
+            }
+        }
+        sleep(180);
+        if (time() == 1473392866) {
+            $flag = false;
+        }
+    }
 
 }
 doLottery();
